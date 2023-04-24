bb6aac2bad8811dd5ec2ea2478902321a9a9f509
James <jamesrobertsongroves@gmail.com>
2023-04-24 14:59:43 +0100

    seeds and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,14 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_417_233_601) do
-  create_table 'movies', force: :cascade do |t|
-    t.string 'title'
-    t.string 'rating'
-    t.decimal 'total_gross'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.text 'description'
-    t.date 'released_on'
+ActiveRecord::Schema[7.0].define(version: 2023_04_24_093350) do
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.string "rating"
+    t.decimal "total_gross"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "description"
+    t.date "released_on"
+    t.string "director"
+    t.string "duration"
+    t.string "image_file_name", default: "placeholder.png"
   end
+
 end
