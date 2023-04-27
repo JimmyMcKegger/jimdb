a1a09f7190a8a3e7c4beb172d2fef7d7d67a716d
James <jamesrobertsongroves@gmail.com>
2023-04-27 13:14:47 +0100

    model validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 class Movie < ApplicationRecord
+  RATINGS = %w(G PG PG-13 R NC-17)
+
+  validates :title, :released_on, :duration, presence: true
+
+  validates :description, length: { minimum: 25 }
+
+  validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
+
+  validates :rating, inclusion: { in: RATINGS }
 
   def self.released
     where("released_on < ?", Time.now).order(released_on: :desc)
