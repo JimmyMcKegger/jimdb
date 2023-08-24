6f6f774baeface077b5babd1c40ff72c95082b40
James <jamesrobertsongroves@gmail.com>
2023-08-25 00:44:40 +0100

    signin

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   end
 
   resource :session, only: [:new, :create, :desstroy]
-  get 'signin' => 'session#new'
+  get 'signin' => 'sessions#new'
 
   resources :users
   get 'signup' => 'users#new'
