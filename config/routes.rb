af9a5f29717c282c326af4eb7509fbcd65324292
James <jamesrobertsongroves@gmail.com>
2023-08-26 23:20:16 +0100

    sign out

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     resources :reviews
   end
 
-  resource :session, only: [:new, :create, :desstroy]
+  resource :session, only: [:new, :create, :destroy]
   get 'signin' => 'sessions#new'
 
   resources :users
