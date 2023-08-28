657f6ebfb3ad752a9c3aa92781b1fc3efed2c86e
James <jamesrobertsongroves@gmail.com>
2023-08-28 23:25:26 +0100

    many-to-many user-reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,10 +2,9 @@
 
 class Review < ApplicationRecord
   belongs_to :movie
+  belongs_to :user
 
-  validates :name, presence: true
-
-  validates :comment, length: { minimum: 4, message: 'must be longer' }
+  validates :comment, length: { minimum: 4, message: 'Comments should be longer...' }
 
   validates :stars,
             numericality: { only_integer: true, greater_than: 0, less_than: 6, message: 'must be between 1 and 5' }
