df39c4ec6c911c8b5c084815b9598e6543f7fa85
James <jamesrobertsongroves@gmail.com>
2023-08-29 08:54:07 +0100

    added favourites

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   has_many :reviews, dependent: :destroy
 
+  has_many :favourites, dependent: :destroy
+
   validates :name, presence: true
 
   validates :email, format: { with: /\S+@\S+/ },
