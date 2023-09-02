3d148ab862c9f873d3dff62cf31d029fdb01e879
James <jamesrobertsongroves@gmail.com>
2023-09-02 12:36:23 +0100

    fave and unfave

--- app/helpers/favourites_helper.rb
+++ app/helpers/favourites_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module FavouritesHelper
+
+  def fave_or_unfave_button(movie, favourite)
+    if favourite.present?
+      button_to "♡ Unfave", movie_favourite_path(movie, favourite), method: :delete
+    else
+      button_to "♥️ Fave", movie_favourites_path(movie)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :favourites
+
   root 'movies#index'
 
   resources :movies do
     resources :reviews
+    resources :favourites
   end
 
   resource :session, only: %i[new create destroy]
